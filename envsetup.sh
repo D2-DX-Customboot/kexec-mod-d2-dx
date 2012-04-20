53f464fc981c22d25e394d2ab89ab58b4d371c14
Matt Parnell <parwok@gmail.com>
2012-04-19 21:59:29 -0700

    fixes and improve gitignore...one warning left to fix

--- envsetup.sh
+++ envsetup.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 # Path to your android ndk ARM build tools
-export PATH=/opt/android-ndk/toolchains/arm-linux-androideabi-4.4.3/prebuilt/linux-x86/bin/:$PATH
-# The COMPILED kernel source location ...make sure not to just use the default config
+export NDK=/opt/android-ndk/toolchains/arm-linux-androideabi-4.4.3/prebuilt/linux-x86/bin/
+
+# Our bash PATH
+export PATH=$NDK:$PATH
+
+# The COMPILED kernel source location
 export KERNDIR=/media/disk/android/kernel/omap3
 
 # Basic stuff
