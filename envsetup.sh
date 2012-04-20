e4bb88db6443dd253a18b38c45223b5fa8bb30df
Matt Parnell <parwok@gmail.com>
2012-04-19 20:02:41 -0700

    cleanup and move and add comments

--- envsetup.sh
+++ envsetup.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+# Path to your android ndk ARM build tools
 export PATH=/opt/android-ndk/toolchains/arm-linux-androideabi-4.4.3/prebuilt/linux-x86/bin/:$PATH
+# The COMPILED kernel source location ...make sure not to just use the default config
+export KERNDIR=/media/disk/android/kernel/omap3
+
+# Basic stuff
 export CC=arm-linux-androideabi-
 export CROSS_COMPILE=arm-linux-androideabi-
 export ARCH=arm
-export KERNDIR=/media/disk/android/kernel/omap3
